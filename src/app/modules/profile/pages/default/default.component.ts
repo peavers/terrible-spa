55069700c4f6b0730b564ac63e7460fd4bddf976
Chris Turner <chris@forloop.space>
2020-01-24 12:22:52 +0000

    wip: adding basic skeleton for HTML

--- src/app/modules/profile/pages/default/default.component.ts
+++ src/app/modules/profile/pages/default/default.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { AuthService } from "../../../../core/services/auth.service";
 import { User } from "firebase";
 
 @Component({
   selector: "app-default",
   templateUrl: "./default.component.html",
-  styleUrls: ["./default.component.scss"]
+  styleUrls: ["./default.component.scss"],
+  encapsulation: ViewEncapsulation.None
 })
 export class DefaultComponent implements OnInit {
   user: User;
+
+  timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
   constructor(private authService: AuthService) {
   }
--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared';
 import { DefaultComponent } from './pages/default/default.component';
 import { RoutingModule } from './profile.routing';
+import { MaterialModule } from "../../shared/material.module";
 
 @NgModule({
   declarations: [DefaultComponent],
-  imports: [SharedModule, RoutingModule],
+  imports: [SharedModule, RoutingModule, MaterialModule],
   exports: [],
   entryComponents: []
 })
