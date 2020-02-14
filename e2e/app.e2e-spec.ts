44f238c9f0d087c5df1bceda26187f8e203b6ed4
Chris Turner <chris@forloop.space>
2020-02-14 18:33:01 +0000

    fix: manual upgrade

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { TerribleSpaPage } from './app.po';
+
+describe('terrible-spa App', function() {
+  let page: TerribleSpaPage;
+
+  beforeEach(() => {
+    page = new TerribleSpaPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
