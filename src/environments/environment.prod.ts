861dbf66fd219e7423ed41fb7185452a31f6560e
Chris Turner <chris@forloop.space>
2020-02-14 19:05:38 +0000

    fix: add place holder for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,16 @@
 export const environment = {
   production: true,
 
+  api: 'http://localhost:8080',
+
   firebase: {
-    apiKey: "AIzaSyCDTdFqdSJUYpB3y0pcC9Iiw2vUqltCenU",
-    authDomain: "terrible-spa.firebaseapp.com",
-    databaseURL: "https://terrible-spa.firebaseio.com",
-    projectId: "terrible-spa",
-    storageBucket: "terrible-spa.appspot.com",
-    messagingSenderId: "11119644174",
-    appId: "1:11119644174:web:5d41387b07b24c3a36b65f",
-    measurementId: "G-JKVFLY2J9W"
+    apiKey: 'AIzaSyCDTdFqdSJUYpB3y0pcC9Iiw2vUqltCenU',
+    authDomain: 'terrible-spa.firebaseapp.com',
+    databaseURL: 'https://terrible-spa.firebaseio.com',
+    projectId: 'terrible-spa',
+    storageBucket: 'terrible-spa.appspot.com',
+    messagingSenderId: '11119644174',
+    appId: '1:11119644174:web:5d41387b07b24c3a36b65f',
+    measurementId: 'G-JKVFLY2J9W'
   }
 };
