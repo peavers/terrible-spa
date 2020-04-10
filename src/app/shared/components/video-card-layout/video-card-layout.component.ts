d748b6bd9822ef2b8d12de3ff05534c386bb7a59
Chris Turner <chris@forloop.space>
2020-04-10 12:13:16 +0100

    feature: Fade in video cards

--- src/app/shared/components/video-card-layout/video-card-layout.component.ts
+++ src/app/shared/components/video-card-layout/video-card-layout.component.ts
@@ -1,11 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { MediaFile } from '../../../core/domain/modules';
 import { Router } from '@angular/router';
+import {trigger, style, animate, transition} from '@angular/animations';
 
 @Component({
   selector: 'app-video-card-layout',
   templateUrl: './video-card-layout.component.html',
-  styleUrls: ['./video-card-layout.component.scss']
+  styleUrls: ['./video-card-layout.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate(150, style({opacity: 1}))
+      ])
+    ])
+  ]
 })
 export class VideoCardLayoutComponent {
   THUMBNAIL_POSITION = 4;
